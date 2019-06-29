bbd8683c01f5d14eef7ef409be52c35cc2e42eda
Naman Avasthi <naman.avasthi@gmail.com>
2019-06-28 22:59:28 -0700

    initial commit. project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+export default function App() {
+  return (
+    <div>
+      <h1>My React Table App</h1>
+    </div>
+  );
+}
+
+render(<App />, document.getElementById("root"));
